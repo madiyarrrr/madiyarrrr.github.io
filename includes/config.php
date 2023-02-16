c865c0eb29b3a464aaac7a90b304ae76a869fd69
madiyar <94220570+madiyarrrr@users.noreply.github.com>
2023-02-16 15:23:31 +0600

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = array(
+    'title' => 'Блог IT_Минималиста Мадияр Маселбек',
+    'vk_url' => 'http://vk.com/madiyarmasselbek',
+    'db' => array(
+        'server' => 'sql110.ezyro.com',
+        'username' => 'ezyro_33042201',
+        'password' => 'na1vl4h9ss',
+        'name' => 'ezyro_33042201_register'
+    )
+);
+
+require "db.php";
